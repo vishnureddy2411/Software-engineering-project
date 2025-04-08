c4119b700c30f504983d68ea9ea8044eaabecf0a
JahnaviChowdary06 <battajahnavichowdary@gmail.com>
2025-04-07 23:04:04 -0500

    updated by JahnaviChowdary06

--- indoor_sports/reports/urls.py
+++ indoor_sports/reports/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('daily/', views.daily_report, name='daily_report'),
     path('weekly/', views.weekly_report, name='weekly_report'),
     path('monthly/', views.monthly_report, name='monthly_report'),
     path('yearly/', views.yearly_report, name='yearly_report'),
