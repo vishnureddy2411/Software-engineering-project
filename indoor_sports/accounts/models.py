e967d7e3f60ba187572abf686693450658906b42
Vishnu <vsvvjobapp@gmail.com>
2025-04-25 15:35:52 -0500

    updated by vishnu

--- indoor_sports/accounts/models.py
+++ indoor_sports/accounts/models.py
@@ -114,6 +114,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
 
 
 # ----------------------------------------------------------------------
--- indoor_sports/indoor_sports/settings.py
+++ indoor_sports/indoor_sports/settings.py
@@ -108,6 +108,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
