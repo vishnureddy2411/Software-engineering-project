6c838a3794a894758dccc7f870afb1b503708c56
Kalyan <kalyandel04@gmail.com>
2025-04-25 05:38:37 -0500

    updated by Kalyan

--- indoor_sports/dashboards/views.py
+++ indoor_sports/dashboards/views.py
@@ -179,7 +179,7 @@ def edit_profile(request):
     }
     return render(request, 'edit_profile.html', context)
 
-@login_required
+
 def edit_profile_admin(request):
     # Get the logged-in user (assuming the user is an instance of the Admin model)
     admin = request.user
